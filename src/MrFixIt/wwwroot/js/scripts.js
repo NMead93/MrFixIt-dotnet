1eeaf3e99e82ff9fa233f4c5baa0600d2c7535bf
Nicholas Mead <nixter11@uw.edu>
2017-05-05 08:40:22 -0700

    add first ajax script and update action

--- src/MrFixIt/wwwroot/js/scripts.js
+++ src/MrFixIt/wwwroot/js/scripts.js
@@ -1,1 +1,11 @@
-﻿var test = "it works";
+﻿$(document).ready(function () {
+    $('.hello-ajax').click(function () {
+        $.ajax({
+            type: 'GET',
+            url: '@Url.Action("HelloAjax")',
+            success: function (result) {
+                $('#result1').html(result);
+            }
+        });
+    });
+});
