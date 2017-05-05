075aac346a65e49a151cd3cd71c1361dc2ce5987
Nicholas Mead <nixter11@uw.edu>
2017-05-05 11:02:25 -0700

    FINALLY fix ajax request for claiming jobs. Stupid javascript naming conventions

--- src/MrFixIt/wwwroot/js/scripts.js
+++ src/MrFixIt/wwwroot/js/scripts.js
@@ -1,11 +1,14 @@
 ﻿$(document).ready(function () {
     $('.claim').click(function () {
+        var clickedId = this.id
         $.ajax({
             type: 'POST',
-            data: { jobId: this.id },
+            data: { jobId: clickedId },
+            dataType: 'json',
             url: '@Url.Action("Claim", "Jobs")',
             success: function (result) {
-                $()
+                $(".name").text(result.Worker.FirstName);
+                $(".action-row").empty().append("<button id=" + result.JobId + " class='btn btn-success btn-xs finish'><span class='glyphicon glyphicon-plus'></span> Finish</button>");
             }
         });
     });
